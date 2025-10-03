a52ec3f57be965ae6b63193b18eb7d86c85986fe
ggabmartins <gabriellm1805@gmail.com>
2025-10-03 19:05:32 -0300

    feat: Adicionado componente inicial da página de login.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,18 @@
-export default function Login() {
+export default function LoginPage() {
   return (
     <div>
-      <h1 className="text-2xl">Página de Login</h1>
-    </div>
-  );
+      <h2>Entrar</h2>
+      <form>
+        <div>
+          <label htmlFor="nomeUsuario">Nome de Usuário</label>
+          <input id="nomeUsuario" type="text" />
+        </div>
+        <div>
+          <label htmlFor="email">E-mail</label>
+          <input id="email" type="email" />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
 }
