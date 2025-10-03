dfcc15ea3a51859bd7359bc1dd2ad9cc1ce8d0c9
ggabmartins <gabriellm1805@gmail.com>
2025-10-03 19:09:52 -0300

    style: estilização inicial do form de login.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,10 @@
 export default function LoginPage() {
   return (
-    <div>
-      <h2>Entrar</h2>
-      <form>
+    <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md">
+      <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
+        Entrar
+      </h2>
+      <form className="space-y-6">
         <div>
           <label htmlFor="nomeUsuario">Nome de Usuário</label>
           <input id="nomeUsuario" type="text" />
@@ -11,7 +13,9 @@ export default function LoginPage() {
           <label htmlFor="email">E-mail</label>
           <input id="email" type="email" />
         </div>
-        <button type="submit">Entrar</button>
+        <button type="submit">
+          Entrar
+        </button>
       </form>
     </div>
   );
